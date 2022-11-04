4966924fd81178ac56bfd5bdbf3ed15cd61d11cc
atifkarim <atif0902082@gmail.com>
2022-11-04 18:10:44 +0100

    building project with pybind11

--- test_script.sh
+++ test_script.sh
@@ -1,9 +1,7 @@
 echo "Hi I am Atif"
-cd ..
-mkdir Test
-echo "2 time tree done"
-tree
-cd ..
-cd ..
-echo "3 time tree done"
+git clone https://github.com/pybind/pybind11.git ../forked_repo/pybind11
+chmod +x run_project.sh
+./run_project.sh
+rm -rf build
+echo "Showing All"
 tree
