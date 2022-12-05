f7d4b6c1f025183d08423eab99810c51a9166e5c
atifkarim <atif0902082@gmail.com>
2022-12-05 23:08:10 +0100

    abstract class is included

--- include/base.hpp
+++ include/base.hpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <string>
 
+template <typename T1 = std::string, typename T2 = int>
+class Image_Base{
+public:
+
+	Image_Base(T1 image_id_ = "No Name"): image_base_id{image_id_}{}
+	virtual ~Image_Base() {}
+	virtual int weight_kg(T2 weight) const = 0;
+
+protected:
+	T1 image_base_id;
+};
+
 class Image_Core{
 	public:
 		Image_Core () = delete;
--- src/pybinded_file/py_binded.cpp
+++ src/pybinded_file/py_binded.cpp
@@ -4,6 +4,35 @@
 
 namespace py = pybind11;
 
+template <typename T1 = std::string, typename T2 = int>
+void Pybind_Image_Base(py::module &m, const std::string& typestr1, const std::string& typestr2)
+{
+	using IMAGE_BASE_CONSTR = Image_Base<T1, T2>;
+
+	class PyVirtualClass_Abstract : public Image_Base<T1, T2>
+	{
+	public:
+		using Image_Base<T1, T2>::Image_Base;
+		typedef Image_Base<T1, T2> macro_type;
+
+		// Trampoline (need one for each virtual function)
+		int weight_kg(T2 weight) const override
+		{
+		PYBIND11_OVERRIDE_PURE(
+			int,           // Function Return type
+			macro_type,    // Class Type where this function is declared
+			weight_kg,     // Name of the function. Same name will be used in driver code also
+			weight         // Function Argument
+			);
+		}
+	};
+
+	/// Create template instance and bind it as python function including the functions. In addition set the default reference pointer type to shared_ptr in C++ instead of unique_ptr (default).
+	py::class_<Image_Base<T1, T2>, PyVirtualClass_Abstract>(m, "Image_Base")
+	.def(py::init<std::string>())
+	.def("weight_kg", &Image_Base <T1, T2>::weight_kg);
+}
+
 PYBIND11_MODULE(pybind_cpp_module, m)
 {
 	py::class_<Image_Core>(m, "Image_Core")
@@ -17,4 +46,6 @@ PYBIND11_MODULE(pybind_cpp_module, m)
 	Image_Process
 	    .def(py::init<std::string, std::string>())
 	    .def("Show_Date", &Image_Process::Show_Date);
+
+	Pybind_Image_Base<std::string, int>(m, "std::string", "int");
 }
