c322a690da874941ae34cb6183d1234d3b91f067
atifkarim <atif0902082@gmail.com>
2023-09-05 23:09:18 +0200

    Add generic python path to build and execute py project

--- run_project.sh
+++ run_project.sh
@@ -1,7 +1,7 @@
 rm -rf build/ bin/ lib/
 rm example/py/*.so
 mkdir build && cd build
-cmake .. -DPYTHON_EXECUTABLE=/usr/bin/python3.8 && make
+cmake .. -DPYTHON_EXECUTABLE=$(which python) && make
 cd ../bin/
 ./pybind_cpp_exec
 
@@ -9,4 +9,4 @@ cd ..
 cp lib/*.so example/py/
 
 cd example/py/
-python3.8 main.py
+$(which python) main.py
